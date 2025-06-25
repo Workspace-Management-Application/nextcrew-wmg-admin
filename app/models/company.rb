10aa04ddad3cc4b7b5bcfa05e397888c581f3250
Nandan Agrawal <nandanagrawal@srnext.in>
2025-06-25 19:16:34 +0530

    minor fix

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,10 +3,12 @@ class Company < ApplicationRecord
   has_many :users, through: :company_users
   has_many :company_workspaces
   has_many :workspaces, through: :company_workspaces
-  has_many :day_passes, optional: true
+  has_many :day_passes
 
   enum :status, { active: 'active', inactive: 'inactive', pending: 'pending', approved: 'approved' }, default: :pending
 
   validates :name, :email, :phone_number, :address, presence: true
   validates :phone_number, uniqueness: true
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "invalid email format" }
+  validates :phone_number, format: { with: /\A(\+?\d{1,3}[- ]?)?\(?\d{1,4}?\)?[- ]?\d{1,4}[- ]?\d{1,4}\z/, message: "invalid phone number format" }
 end
