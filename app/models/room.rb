d4a8cf80848b651cd4c6980f2fb1389ad7d075e3
Nandan Agrawal <nandanagrawal@srnext.in>
2025-06-25 19:55:41 +0530

    minor fix in room model

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,8 @@
 class Room < ApplicationRecord
   belongs_to :workspace
 
-  validates :name, :category, :capacity, presence: true
+  validates :name, :category, presence: true
+  validates :capacity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :whiteboard, :projector, :is_available, inclusion: { in: [true, false] }
 
   def is_occupied?
